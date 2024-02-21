568726620af864fbab55815339c1af89c037f560
Yuma Ochi <ochiyuma.ochi@gmail.com>
2024-02-22 00:54:03 +0900

    update

--- app.py
+++ app.py
@@ -3,9 +3,38 @@ import requests
 from instaloader import Instaloader, Profile
 from facebook_scraper import get_profile
 from typing import Dict
-
+import pandas as pd
+import plotly.express as px
+import phonenumbers
+from phonenumbers import geocoder
 from pdf import create_pdf
 import json
+
+def display_map(country):
+    df = pd.DataFrame({
+            'country': [country],
+                'iso_alpha': ['THA'],
+                    'value': [1],  # You can adjust the values based on what you want to represent (e.g., population, GDP)
+                    })
+
+    # Creating the Choropleth map
+    fig = px.choropleth(df, locations="iso_alpha",
+                                            color="value",  # value that you want to color-code
+                                            hover_name="country",  # column to add to hover information
+                                            color_continuous_scale=px.colors.sequential.Plasma)
+
+    # Displaying the map in the Streamlit app
+    st.plotly_chart(fig)
+
+def country_from_number(raw):
+    phonenumber = "+" + raw[:2] +  " " + raw[2:]
+
+    # Parse in the phone numbers from the txt file later
+    phone_number = phonenumbers.parse(phonenumber)
+    country = geocoder.description_for_number(phone_number, "en")
+
+    print(f"The country of the phone number is: {country}")
+    return country
 
 st.title("Scam Detector")
 
@@ -19,13 +48,13 @@ def open_json(filepath: str) -> Dict:
 
 info = open_json("data.json")
 
-username = col1.text_input("username", "wassupkanti")
+username = col1.text_input("username", "nahid_danica")
 
 col2.write("")
 num_of_cols = 3
 
-if col2.button("Run"):
 
+if col2.button("Run"):
     filepath = "telegram.json"
     text = f"""
     ## Telegram Details
@@ -45,6 +74,7 @@ if col2.button("Run"):
     path = "data.pdf"
     create_pdf(info, path)
 
+
     with open(path, "rb") as f:
 
         btn = st.download_button(
@@ -53,3 +83,6 @@ if col2.button("Run"):
                 file_name=path,
         )
 
+    location = country_from_number(info["Instagram"]["Phone Number"])
+    st.write(f"The country of the phone number is: {location}")
+    display_map(location)
